0c362d98590fafde38a221df5ccc5414da0ba48a
tavi toporjinschi <vasike@gmail.com>
2022-05-20 17:56:55 +0300

    LDP-1321: PHPStan fixes \Drupal calls should be avoided in classes, use dependency injection instead.

--- src/Plugin/views/argument_default/EntityRenderHistory.php
+++ src/Plugin/views/argument_default/EntityRenderHistory.php
@@ -22,28 +22,40 @@ class EntityRenderHistory extends ArgumentDefaultPluginBase {
   use EntityRenderHistoryTrait;
 
   /**
-   * The service container.
-   *
-   * @var \Symfony\Component\DependencyInjection\ContainerInterface
-   */
-  protected $container;
-
-  /**
-   * Constructs a container aware event dispatcher.
-   *
-   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
-   *   The service container.
-   */
-  public function __construct(ContainerInterface $container) {
-    $this->container = $container;
-  }
-
-  /**
    * The entity type manager.
    *
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
+
+  /**
+   * Constructs a Raw object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * Sets the entity type.
@@ -65,9 +77,6 @@ class EntityRenderHistory extends ArgumentDefaultPluginBase {
    *   The entity type manager.
    */
   public function getEntityTypeManager() {
-    if (!isset($this->entityTypeManager)) {
-      $this->entityTypeManager = $this->container->get('entity_type.manager');
-    }
     return $this->entityTypeManager;
   }
 
